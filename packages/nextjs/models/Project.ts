139ba6e32d0bad190a1749737d35ef40e54ed566
Paul Gadi <paulgadi@gmail.com>
2024-01-04 23:48:41 -0500

    add template mongodb code from OP-RetroPGF3-Discovery-Voting

---
+++ packages/nextjs/models/Project.ts
@@ -0,0 +1,92 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface ContributionLink {
+  type: string;
+  url: string;
+  description: string;
+}
+
+interface ImpactMetrics {
+  description: string;
+  number: number;
+  url: string;
+}
+
+interface FundingSource {
+  type: string;
+  currency: string;
+  amount: number;
+  description: string;
+}
+
+export interface IProject {
+  _id: string; // You can use string as the type for _id
+  name: string;
+  url: string;
+  profileImageUrl: string;
+  bannerImageUrl: string;
+  websiteUrl: string;
+  bio: string;
+  applicantType: string;
+  contributionDescription: string;
+  contributionLinks: ContributionLink[];
+  category: string[];
+  impactDescription: string;
+  impactMetrics: ImpactMetrics[];
+  fundingSources: FundingSource[];
+  payoutAddress: string;
+  understoodKYCRequirements: boolean;
+  understoodFundClaimPeriod: boolean;
+  certifiedNotDesignatedOrSanctionedOrBlocked: boolean;
+  certifiedNotSponsoredByPoliticalFigureOrGovernmentEntity: boolean;
+  certifiedNotBarredFromParticipating: boolean;
+  ownerName: string;
+}
+
+export type ProjectDocument = IProject & Document;
+
+export const projectSchema = new Schema<ProjectDocument>({
+  name: String,
+  url: String,
+  profileImageUrl: String,
+  bannerImageUrl: String,
+  websiteUrl: String,
+  bio: String,
+  applicantType: String,
+  contributionDescription: String,
+  contributionLinks: [
+    {
+      type: String,
+      url: String,
+      description: String,
+    },
+  ],
+  category: [String],
+  impactDescription: String,
+  impactMetrics: [
+    {
+      description: String,
+      number: Number,
+      url: String,
+    },
+  ],
+  fundingSources: [
+    {
+      type: String,
+      currency: String,
+      amount: Number,
+      description: String,
+    },
+  ],
+  payoutAddress: String,
+  understoodKYCRequirements: Boolean,
+  understoodFundClaimPeriod: Boolean,
+  certifiedNotDesignatedOrSanctionedOrBlocked: Boolean,
+  certifiedNotSponsoredByPoliticalFigureOrGovernmentEntity: Boolean,
+  certifiedNotBarredFromParticipating: Boolean,
+  ownerName: String,
+});
+
+const Project = mongoose.models.Projects || mongoose.model("Projects", projectSchema);
+
+export default Project;
---
+++ packages/nextjs/pages/api/suggestedProjects.ts
@@ -0,0 +1,28 @@
+import { ObjectId } from "mongodb";
+import type { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "~~/lib/dbConnect";
+import Project, { IProject } from "~~/models/Project";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Method not allowed." });
+  }
+  await dbConnect();
+  /*
+  const { category, currentProjectId } = req.query;
+  console.log(category, currentProjectId);
+  
+  const suggestedProjects: IProject[] = await Project.find({
+    impactCategory: category,
+    _id: { $ne: new ObjectId(currentProjectId as string) },
+  }).limit(3);
+  */
+
+  const suggestedCategory = "Education";
+  const suggestedProjects: IProject[] = await Project.find({
+    category: suggestedCategory,
+  }).limit(3);
+
+  console.log(suggestedProjects);
+  return res.status(200).json(suggestedProjects);
+}
